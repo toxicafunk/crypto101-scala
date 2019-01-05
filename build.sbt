7742edb7bb3b4edd95eacd7b6375d64892ff3cae
Eric Rodriguez <eric.rodriguez@eniro.com>
2019-01-05 18:49:50 +0100

    Update scala version and formatting

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 lazy val root = (project in file(".")).
   settings(
     name := "Crypto101",
-    scalaVersion := "2.11.7"
+    scalaVersion := "2.12.8"
   )
--- src/main/scala/crypto101/OneTimeCypher.scala
+++ src/main/scala/crypto101/OneTimeCypher.scala
@@ -1,10 +1,12 @@
 package crypto101
 
+import scala.util.matching.Regex
+
 class OneTimeCypher(key: Int) { // 'key' gets a setter, 'val key' creates a getter and a setter,
   def encode(plaintext: String): String =
     plaintext.getBytes.map(_ ^ key).foldLeft("")((acc, cur) => acc + cur.toString)
 
-  val pat = s".{${key.toString.length}}".r
+  val pat: Regex = s".{${key.toString.length}}".r
 
   def decode(ciphertext: String): String = {
     helper(ciphertext).map(_.toInt ^ key).map(_.toChar).mkString
@@ -32,7 +34,7 @@ object OneTimeCypher {
   }
 
   implicit class StringUtils(val xs: List[String]) {
-    def mostFrequentByte: (String, Int) = xs.groupBy(s => s).map( (elem) => elem._1 -> elem._2.length ).maxBy(_._2)
+    def mostFrequentByte: (String, Int) = xs.groupBy(s => s).map( elem => elem._1 -> elem._2.length ).maxBy(_._2)
   }
 
   def main (args: Array[String] ): Unit = {
@@ -41,13 +43,13 @@ object OneTimeCypher {
     val p1 = "This is a test"
     val p2 = "Another 1 test"
 
-    val diff1 = cipher.reusedKey(true)(p1, p2)
+    val diff1 = cipher.reusedKey(applyEncode = true)(p1, p2)
     println(s"From 2 plaintexts: $diff1")
 
     val c1 = cipher.encode(p1)
     val c2 = cipher.encode(p2)
 
-    val diff2 = cipher.reusedKey(false)(c1, c2)
+    val diff2 = cipher.reusedKey(applyEncode = false)(c1, c2)
     println(s"From 2 ciphertexts: $diff2")
 
     println(s"c1: $c1\nc2: $c2")
@@ -60,25 +62,25 @@ object OneTimeCypher {
     val c = "383366376383"
     val p = "test"
 
-    println((cipher.helper(c).map(_.toInt) xor p.getBytes.map(Byte.byte2int(_))))
+    println(cipher.helper(c).map(_.toInt) xor p.getBytes.map(Byte.byte2int))
 
     val d = cipher.decode(c1.mkString)
     println(d )
 
-    println(('a' to 'z').map(Char.char2int(_)))
+    println(('a' to 'z').map(Char.char2int))
 
     val p3 = "Miners process transactions on the blockchain. What this really means is that miners collect all the transactions into a block and then validate and verify the information based on preset rules."
     val p4 = "The target is self adjusting. That is, it has a bit of intelligence. In the bitcoin blockchain, the target is re-adjusted every 2 weeks so that on average, the target is achieved every 10 minutes."
 
     val c3 = cipher.encode(p3)
     val c4 = cipher.encode(p4)
-    val reuse = cipher.reusedKey(false)(c3, c4)
+    val reuse = cipher.reusedKey(applyEncode = false)(c3, c4)
     println(s"$reuse")
     val i0 = reuse.indexOf(0)
     println(s"Position $i0 for first text is ${cipher.helper(c3)(i0)} (${p3(i0)})")
     println(s"Position $i0 for second text is ${cipher.helper(c4)(i0)} (${p4(i0)})")
 
     val x = cipher.helper(c3)(i0).toInt ^ p3(i0)
-    println(s"XOR of ${cipher.helper(c3)(i0)} and ${p3(i0)} = ${x} (key)")
+    println(s"XOR of ${cipher.helper(c3)(i0)} and ${p3(i0)} = $x (key)")
   }
 }
